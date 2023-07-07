78b6383065e976c91b5687046fd93e4e512d80c6
EdEsquivel <edgaresquivel0615@gmail.com>
2023-07-07 00:51:53 -0500

    Added Chakra UI
    Added Chakra and some of its components to style app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
-import logo from "./assets/logo-spacex.png";
+import { Heading, Box, Image, Flex, Text, Spacer, Tag } from "@chakra-ui/react";
 import * as API from "./services/launches";
+import logo from "./assets/logo-spacex.png";
 
 export function App() {
   const [launches, setLaunches] = useState([]);
@@ -11,15 +12,33 @@ export function App() {
 
   return (
     <>
-      <img src={logo} width={300} />
-      <h1>SpaceX Launches</h1>
-      <ul>
+      <Image m={1} src={logo} width={300} />
+      <Heading as="h1" size="lg" m={4}>
+        SpaceX Launches
+      </Heading>
+      <section>
         {launches.map((launch) => (
-          <li key={launch.flight_number}>
+          <Box
+            key={launch.flight_number}
+            bg="gray.100"
+            p={4}
+            m={4}
+            borderRadius="lg"
+          >
+            <Flex>
+              <Text fontSize="2xl">
+                Mission <strong>{launch.mission_name}</strong> (
+                {launch.launch_year})
+              </Text>
+              <Spacer />
+              <Tag p={4} colorScheme={launch.launch_success ? "green" : "red"}>
+                {launch.launch_success ? "success" : "failure"}
+              </Tag>
+            </Flex>
             {launch.mission_name} ({launch.launch_year})
-          </li>
+          </Box>
         ))}
-      </ul>
+      </section>
     </>
   );
 }
