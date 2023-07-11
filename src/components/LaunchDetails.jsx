69130673d2e2ebe29415c90175e159fd24820133
EdEsquivel <edgaresquivel0615@gmail.com>
2023-07-11 01:56:06 -0500

    Added icons and links
    External links to articles, Wikipedia and Youtube were added to "More Details".

--- src/components/LaunchDetails.jsx
+++ src/components/LaunchDetails.jsx
@@ -1,6 +1,19 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { Box, Flex, Text, Spacer, Tag, Spinner } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Text,
+  Spacer,
+  Tag,
+  Spinner,
+  Icon,
+  Button,
+  Image,
+  Divider,
+  AbsoluteCenter,
+} from "@chakra-ui/react";
+import { BsWikipedia, BsYoutube, BsBook } from "react-icons/bs";
 import * as API from "../services/launches";
 
 export function LaunchDetails() {
@@ -42,10 +55,49 @@ export function LaunchDetails() {
             </Flex>
           </Box>
           <Box>
-            <Text as="b" fontSize="xl" color="blue">
-              Details:
-            </Text>
+            <Box position="relative" padding="5">
+              <Divider />
+              <AbsoluteCenter px="5">
+                <Text as="b" fontSize="xl">
+                  Details
+                </Text>
+              </AbsoluteCenter>
+            </Box>
             <Text>{launch?.details}</Text>
+          </Box>
+          <Box mt={5}>
+            <Flex>
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href={launch?.links?.article_link}
+              >
+                <Button>
+                  <Icon boxSize={8} as={BsBook} />
+                  <Text ml={1}>Learn more</Text>
+                </Button>
+              </a>
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href={launch?.links?.wikipedia}
+              >
+                <Button>
+                  <Icon boxSize={8} as={BsWikipedia} />
+                  <Text ml={1}>Wikipedia</Text>
+                </Button>
+              </a>
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href={launch?.links?.video_link}
+              >
+                <Button>
+                  <Icon ml={3} boxSize={8} color="red.600" as={BsYoutube} />
+                  <Text ml={1}>Youtube</Text>
+                </Button>
+              </a>
+            </Flex>
           </Box>
         </>
       )}
